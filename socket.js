2bda059d4a543289bf395e549a53276c4dd1c1be
Russell Feldhausen <russfeldh@gmail.com>
2022-06-27 22:47:08 -0500

    Helper Presence

--- socket.js
+++ socket.js
@@ -1,7 +1,11 @@
+// Load Libraries
 const { Server } = require('socket.io')
 const registerQueueHandlers = require('./sockets/queues')
 const socketToken = require('./middlewares/socket-token')
 const socketQueue = require('./middlewares/socket-queue')
+
+// Load Models
+const User = require('./models/user')
 
 const io = new Server({
   // TODO Fix CORS when deploy
@@ -23,62 +27,49 @@ io.on('connection', connectionEvent)
 let users = {}
 let helpers = {}
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
   try {
+    //console.log(socket.data.user_id + " online on queue " + socket.data.queue_id)
+    if (!helpers[socket.data.queue_id]) {
+      helpers[socket.data.queue_id] = {}
+    }
     if (!users[socket.data.queue_id]) {
       users[socket.data.queue_id] = {}
     }
-    if (!users[socket.data.queue_id][socket.data.user_id]) {
-      users[socket.data.queue_id][socket.data.user_id] = []
-    }
-    users[socket.data.queue_id][socket.data.user_id].push(socket.id)
-    //console.log(socket.data.user_id + " online on queue " + socket.data.queue_id)
     if (socket.data.is_helper) {
-      if (!helpers[socket.data.queue_id]) {
-        helpers[socket.data.queue_id] = {}
-      }
       if (!helpers[socket.data.queue_id][socket.data.user_id]) {
         helpers[socket.data.queue_id][socket.data.user_id] = []
       }
+      helpers[socket.data.queue_id][socket.data.user_id].push(socket.id)
+      const helper = await User.query().findById(socket.data.user_id).select('id', 'name')
+      socket.to('queue-' + socket.data.queue_id).emit(
+        'helper:online',
+        helper
+      )
+    } else {
+      
+      if (!users[socket.data.queue_id][socket.data.user_id]) {
+        users[socket.data.queue_id][socket.data.user_id] = []
+      }
+      users[socket.data.queue_id][socket.data.user_id].push(socket.id)
+      socket.to('queue-' + socket.data.queue_id).emit(
+        'user:online',
+        socket.data.user_id
+      )
     }
-    io.to('queue-' + socket.data.queue_id).emit(
-      'user:online',
-      socket.data.user_id
-    )
+    const helpersOnline = await User.query().findByIds(Object.getOwnPropertyNames(helpers[socket.data.queue_id])).select('id', 'name')
     socket.emit(
       'connected',
-      Object.getOwnPropertyNames(users[socket.data.queue_id])
+      Object.getOwnPropertyNames(users[socket.data.queue_id]),
+      helpersOnline
     )
   } catch (error) {
     // TODO error handling
     //console.log("error connect")
-    //console.log(error)
+    console.log(error)
   }
 
   socket.on('disconnecting', () => {
-    try {
-      var index = users[socket.data.queue_id][socket.data.user_id].findIndex(
-        (u) => u === socket.id
-      )
-      if (index >= 0) {
-        users[socket.data.queue_id][socket.data.user_id].splice(index, 1)
-      } else {
-        //console.log("socket disconnect - socket not found")
-      }
-      //console.log(socket.data.user_id + " offline on queue " + socket.data.queue_id)
-      if (users[socket.data.queue_id][socket.data.user_id].length === 0) {
-        socket
-          .to('queue-' + socket.data.queue_id)
-          .emit('user:offline', socket.data.user_id)
-        delete users[socket.data.queue_id][socket.data.user_id]
-      } else {
-        //console.log("socket disconnect - user still has sockets")
-      }
-    } catch (error) {
-      // TODO error handling
-      //console.log("error disconnect")
-      //console.log(error)
-    }
     if (socket.data.is_helper) {
       try {
         index = helpers[socket.data.queue_id][socket.data.user_id].findIndex(
@@ -91,12 +82,39 @@ io.on('connection', (socket) => {
         }
         //console.log(socket.data.user_id + " offline on queue " + socket.data.queue_id)
         if (helpers[socket.data.queue_id][socket.data.user_id].length === 0) {
+          socket
+            .to('queue-' + socket.data.queue_id)
+            .emit('helper:offline', socket.data.user_id)
           delete helpers[socket.data.queue_id][socket.data.user_id]
         } else {
           //console.log("socket disconnect - user still has sockets")
         }
       } catch (error) {
         // TODO error handling
+        console.log(error)
+      }
+    } else {
+      try{
+        var index = users[socket.data.queue_id][socket.data.user_id].findIndex(
+          (u) => u === socket.id
+        )
+        if (index >= 0) {
+          users[socket.data.queue_id][socket.data.user_id].splice(index, 1)
+        } else {
+          //console.log("socket disconnect - socket not found")
+        }
+        //console.log(socket.data.user_id + " offline on queue " + socket.data.queue_id)
+        if (users[socket.data.queue_id][socket.data.user_id].length === 0) {
+          socket
+            .to('queue-' + socket.data.queue_id)
+            .emit('user:offline', socket.data.user_id)
+          delete users[socket.data.queue_id][socket.data.user_id]
+        } else {
+          //console.log("socket disconnect - user still has sockets")
+        }
+      } catch (error) {
+        // TODO error handling
+        console.log(error)
       }
     }
   })
